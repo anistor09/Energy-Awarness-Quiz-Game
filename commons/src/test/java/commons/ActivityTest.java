c43c10a4ab4cddc61002cca53738e5fdd5429e41
Alexandru Nistor <a.nistor@student.tudelft.nl>
2022-03-05 15:19:10 +0100

    Added before @BeforeEach to avoid code duplication

--- commons/src/test/java/commons/ActivityTest.java
+++ commons/src/test/java/commons/ActivityTest.java
@@ -1,44 +1,52 @@
 package commons;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ActivityTest {
+    private Activity act1;
+    private Activity act2;
+    private Activity act3;
+
+    @BeforeEach
+    public void setup() {
+        act1 = new Activity(1L, "Cook one egg", 10);
+        act2 = new Activity(1L, "Cook one egg", 10);
+        act3 = new Activity(2L, "Cook two egg", 20);
+    }
 
     @Test
     public void checkConstructor() {
-         Activity act1 = new Activity(1L,"Cook one egg",10);
-         assertNotNull(act1);
+        assertNotNull(act1);
     }
+
     @Test
-    public void testEquals(){
-        Activity act1 = new Activity(1L,"Cook one egg",10);
-        Activity act2 = new Activity(1L,"Cook one egg",10);
+    public void testEquals() {
+
         assertTrue(act1.equals(act2));
     }
-    @Test
-    public void testEqualHashCode(){
-        Activity act1 = new Activity(1L,"Cook one egg",10);
-        Activity act2 = new Activity(1L,"Cook one egg",10);
-        assertTrue(act1.hashCode()==act2.hashCode());
-    }
-    @Test
-    public void testNotEquals(){
-        Activity act1 = new Activity(1L,"Cook one egg",10);
-        Activity act2 = new Activity(2L,"Cook two egg",20);
-        assertNotEquals(act1,act2);
-    }
-    @Test
-    public void testNotEqualHashCode(){
-        Activity act1 = new Activity(1L,"Cook one egg",10);
-        Activity act2 = new Activity(2L,"Cook two egg",20);
-        assertNotEquals(act1,act2);
-    }
-    @Test
-    public void testToString(){
-        Activity act1 = new Activity(1L,"Cook one egg",10);
 
-        assertEquals(act1.toString(),"Activity{Id=1, text='Cook one egg', correctAnswer=10}");
+    @Test
+    public void testEqualHashCode() {
+        assertTrue(act1.hashCode() == act2.hashCode());
+    }
+
+    @Test
+    public void testNotEquals() {
+
+        assertNotEquals(act1, act3);
+    }
+
+    @Test
+    public void testNotEqualHashCode() {
+
+        assertNotEquals(act1, act3);
+    }
+
+    @Test
+    public void testToString() {
+        assertEquals(act1.toString(), "Activity{Id=1, text='Cook one egg', correctAnswer=10}");
     }
 }
