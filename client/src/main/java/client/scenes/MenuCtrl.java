0fef975d154a171d654b8b988ef05f47e0f34640
Rafael Borges <R.BragaMedeirosMotaBorges@student.tudelft.nl>
2022-03-05 17:32:30 +0100

    Modified menuCtrl to have all the FXML fields of the scene

--- client/src/main/java/client/scenes/MenuCtrl.java
+++ client/src/main/java/client/scenes/MenuCtrl.java
@@ -1,6 +1,11 @@
 package client.scenes;
 
 import com.google.inject.Inject;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import javax.swing.text.html.ImageView;
 
 
 public class MenuCtrl {
@@ -10,6 +15,29 @@ public class MenuCtrl {
     @Inject
     public MenuCtrl(MainCtrl mainCtrl) {
         this.mainCtrl = mainCtrl;
+    }
+
+    @FXML
+    private Button credits;
+
+    @FXML
+    private Button exit;
+
+    @FXML
+    private ImageView multiIcon;
+
+    @FXML
+    private Button multiPlayer;
+
+    @FXML
+    private ImageView singleIcon;
+
+    @FXML
+    private Button singlePlayer;
+
+    @FXML
+    void exitButton(ActionEvent event) {
+
     }
 
     public void exitButton() {
